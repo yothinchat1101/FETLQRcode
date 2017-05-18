b00cd9d30b9c01c498bf6a3a7f5b471ec813f3a3
YothinC <Yothin.ch1101@gmail.com>
2017-05-18 09:45:56 +0700

    Show Text Ok

--- app/src/main/java/fetl/chatveeratham/yothin/fetlqrcode/ServiceActivity.java
+++ app/src/main/java/fetl/chatveeratham/yothin/fetlqrcode/ServiceActivity.java
@@ -2,12 +2,36 @@ package fetl.chatveeratham.yothin.fetlqrcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ServiceActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView textView;
+    private ImageView barImageView, qrImageView;
+    private ListView listView;
+    private String nameString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service);
+        //Initial View
+        initialView();
+
+        //Show Text
+        nameString = getIntent().getStringExtra("Login");
+        textView.setText(nameString);
+
     }//Main Method
+
+    private void initialView() {
+        textView = (TextView) findViewById(R.id.txtNameLogin);
+        barImageView = (ImageView) findViewById(R.id.imvBarCode);
+        qrImageView = (ImageView) findViewById(R.id.imvQrCode);
+        listView = (ListView) findViewById(R.id.livProduct);
+    }
 }//Main Class
